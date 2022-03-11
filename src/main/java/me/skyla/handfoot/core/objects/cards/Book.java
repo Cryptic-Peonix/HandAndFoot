3daa5e9bcf5b7deca73a5b8440250a3febdc3f66
cclark22 <skylaclark094@gmail.com>
2022-03-11 14:44:08 -0500

    imports optimized

--- src/main/java/me/skyla/handfoot/core/objects/cards/Book.java
+++ src/main/java/me/skyla/handfoot/core/objects/cards/Book.java
@@ -1,8 +1,6 @@
 package me.skyla.handfoot.core.objects.cards;
 
 import me.skyla.handfoot.Main;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import processing.core.PApplet;
 
 import java.awt.*;
